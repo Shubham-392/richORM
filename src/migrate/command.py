1aabce447709bd9c775cf5261679cc7bb8df2736
Shubham <shubham@unifyitc.com>
2025-09-17 23:40:41 +0530

    creating '.yml' file for configuration of richORM

--- src/migrate/command.py
+++ src/migrate/command.py
@@ -1,7 +1,25 @@
 import click
+import yaml
+import os
 
 @click.command()
 @click.version_option(version="0.1.0")
 def cli():
-    """Prints a greeting."""
-    click.echo("I am coming in WORLD !")
+    dumping_data = {
+        "db":{
+            "DRIVER":"adaptor",
+            "DB_NAME":"richorm",
+            "DB_HOST":"localhost",
+            "DB_PORT":3301,
+            "DB_PASSWORD":"localhost@password"
+        },
+        "models":{
+            "directory":"path/to/models/directory"
+        }
+    }
+    
+    currentDirectory  = os.getcwd()
+    with open(f'{currentDirectory}/richorm-config.yml', "w") as configFile:
+        yaml.dump(dumping_data, configFile, default_flow_style=False )
+    
+    
